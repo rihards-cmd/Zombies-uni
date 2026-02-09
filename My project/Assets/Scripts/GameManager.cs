dbec3250575cb37be4ceba77240aa926f0ff1763
VladsMalenkovs <rihardsuka@gmail.com>
2026-02-09 15:49:41 +0200

    zombie file added

---
+++ My project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] zombies;
+    public GameObject selectedZombie;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
